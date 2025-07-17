bd72c328068547dc4495e0383fce6ea7c7ab5ec5
vbshkn <pika_pikabu@vk.com>
2025-07-17 15:00:26 +0700

    Composing OptionPanel

--- app/src/main/java/vbshkn/android/jetmemo/ui/activity/LearnWordsActivity.kt
+++ app/src/main/java/vbshkn/android/jetmemo/ui/activity/LearnWordsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -15,14 +16,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -31,7 +34,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import vbshkn.android.jetmemo.R
-import vbshkn.android.jetmemo.ui.theme.JetMemoTheme
+import vbshkn.android.jetmemo.ui.theme.BorderGrey
+import vbshkn.android.jetmemo.ui.theme.Grey10
+import vbshkn.android.jetmemo.ui.theme.OptionTextGrey
 
 class LearnWordsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,9 +48,11 @@ class LearnWordsActivity : ComponentActivity() {
                     Column(
                         modifier = Modifier
                             .padding(innerPadding)
+                            .background(Color.White)
                     ) {
                         CloseButton()
                         QuestionWord("Galaxy")
+                        OptionPanel()
                     }
                 }
             )
@@ -84,13 +91,101 @@ fun QuestionWord(text: String){
 }
 
 @Composable
+fun Option(
+    number: Int,
+    displayedText: String
+){
+    Button(
+        onClick = {},
+        shape = RoundedCornerShape(20.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+        border = BorderStroke(1.dp, BorderGrey),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                top = 16.dp,
+                bottom = 14.dp
+            )
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                contentPadding = PaddingValues(1.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Grey10,
+                    contentColor = OptionTextGrey
+                ),
+                modifier = Modifier
+                    .size(40.dp)
+            ) {
+                Text(
+                    text = number.toString(),
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    fontFamily = FontFamily(Font(R.font.nunito_semibold)),
+                )
+            }
+            Text(
+                text = displayedText,
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.rubik_regular)),
+                color = OptionTextGrey,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .padding(start = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun OptionPanel(){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = 32.dp,
+                end = 32.dp,
+                top = 80.dp
+            )
+    ){
+        Option(1, "Option")
+        Spacer(Modifier.height(8.dp))
+        Option(2, "Optionononnono")
+        Spacer(Modifier.height(8.dp))
+        Option(3, "Opopo")
+        Spacer(Modifier.height(8.dp))
+        Option(4, "I GOT OPTIONS OH")
+    }
+}
+
+//@Composable
+//@Preview(showBackground = true)
+//private fun CloseButtonPreview(){
+//    CloseButton()
+//}
+
+//@Composable
+//@Preview(showBackground = true)
+//private fun QuestionWordPreview(){
+//    QuestionWord("Galaxy")
+//}
+
+@Composable
 @Preview(showBackground = true)
-private fun CloseButtonPreview(){
-    CloseButton()
+private fun OptionPreview(){
+    Option(1, "Галактика")
 }
 
 @Composable
 @Preview(showBackground = true)
-private fun QuestionWordPreview(){
-    QuestionWord("Galaxy")
+private fun OptionScreenPreview(){
+    OptionPanel()
 }
--- app/src/main/java/vbshkn/android/jetmemo/ui/theme/Color.kt
+++ app/src/main/java/vbshkn/android/jetmemo/ui/theme/Color.kt
@@ -5,7 +5,12 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// custom colors
+
+val BorderGrey = Color(0xFFCCCCD0)
+val OptionTextGrey = Color(0xFF888992)
+val Grey10 = Color(0xFFEEEEEF)
