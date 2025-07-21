cccaacc7d2bfbf34329030a6bcb75ff00efb1a4c
vbshkn <pika_pikabu@vk.com>
2025-07-21 13:41:13 +0700

    Testing UnitList

--- app/src/main/java/vbshkn/android/jetmemo/ui/activity/MainActivity.kt
+++ app/src/main/java/vbshkn/android/jetmemo/ui/activity/MainActivity.kt
@@ -1,18 +1,29 @@
 package vbshkn.android.jetmemo.ui.activity
 
+import android.inputmethodservice.Keyboard.Row
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -21,6 +32,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -42,7 +54,12 @@ class MainActivity : ComponentActivity() {
             Scaffold(
                 topBar = { TopBar() }
             ) { innerPadding ->
-                UnitsLazyRow(innerPadding)
+                Column(
+                    modifier = Modifier.padding(innerPadding)
+                ) {
+                    //YourUnitsLabel()
+                    UnitList()
+                }
             }
         }
     }
@@ -89,13 +106,57 @@ fun TopBar(){
 }
 
 @Composable
-fun UnitsLazyRow(paddingValues: PaddingValues){
-    LazyRow(
+fun YourUnitsLabel(){
+    Row(
+        verticalAlignment = Alignment.Top,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Text(
+            text = stringResource(R.string.your_units),
+            color = VividBlue,
+            fontSize = 20.sp,
+            fontFamily = FontFamily(Font(R.font.nunito_bold)),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 5.dp, top = 5.dp)
+        )
+    }
+}
+
+@Composable
+fun UnitList(){
+    val testData = listOf("ONE", "TWO", "THREE", "ME GUSTAS", "I GOT SO MANY THINGS")
+    LazyColumn (
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
-            .padding(paddingValues)
+            .padding(top = 10.dp)
             .background(Color.White)
             .fillMaxSize()
     ) {
+        items(testData){
+            item -> UnitButton(item)
+        }
+    }
+}
 
+@Composable
+fun UnitButton(text: String){
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.White,
+            contentColor = VividBlue,
+        ),
+        border = BorderStroke(1.dp, VividBlue),
+        shape = RoundedCornerShape(0.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            fontFamily = FontFamily(Font(R.font.nunito_semibold))
+        )
     }
 }
