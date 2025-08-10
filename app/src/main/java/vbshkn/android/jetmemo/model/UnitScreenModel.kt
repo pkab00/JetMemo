a4d79da82ab1e966276c2280751cb719e3610186
vbshkn <pika_pikabu@vk.com>
2025-08-10 13:01:49 +0700

    Implemented SortMode for UnitScreenModel

--- app/src/main/java/vbshkn/android/jetmemo/model/UnitScreenModel.kt
+++ app/src/main/java/vbshkn/android/jetmemo/model/UnitScreenModel.kt
@@ -1,6 +1,8 @@
 package vbshkn.android.jetmemo.model
 
+import android.content.Intent.ShortcutIconResource
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
@@ -8,6 +10,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import vbshkn.android.jetmemo.data.UnitEntity
@@ -18,7 +21,18 @@ class UnitScreenModel(
     private val repository: UnitRepository,
     val unitID: Int
 ) : ViewModel() {
-    val unitWords = repository.getAllFromUnit(unitID)
+    var sortMode = mutableStateOf(SortMode.getNextMode())
+        private set
+    val unitWords = repository.getAllFromUnit(unitID).map { data ->
+        when (sortMode.value) {
+            is SortMode.ByOriginalAsc -> data.sortedBy { it.original }
+            is SortMode.ByOriginalDesc -> data.sortedByDescending { it.original }
+            is SortMode.ByTranslationAsc -> data.sortedBy { it.translation }
+            is SortMode.ByTranslationDesc -> data.sortedByDescending { it.translation }
+            is SortMode.ByTimeAsc -> data.sortedBy { it.createdAt }
+            is SortMode.ByTimeDesc -> data.sortedByDescending { it.createdAt }
+        }
+    }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(5000),
@@ -27,7 +41,8 @@ class UnitScreenModel(
     private val _unitsToAddTo = MutableStateFlow<List<UnitEntity>>(emptyList())
     val unitsToAddTo = _unitsToAddTo.asStateFlow()
 
-    fun addWord(word: WordEntity){
+
+    fun addWord(word: WordEntity) {
         viewModelScope.launch {
             repository.addWord(word, unitID)
         }
@@ -42,25 +57,25 @@ class UnitScreenModel(
         }
     }
 
-    fun editWord(word: WordEntity){
+    fun editWord(word: WordEntity) {
         viewModelScope.launch {
             repository.editWord(word)
         }
     }
 
-    fun deleteWordFromUnit(word: WordEntity){
+    fun deleteWordFromUnit(word: WordEntity) {
         viewModelScope.launch {
             repository.deleteWordFromUnit(word, unitID)
         }
     }
 
-    fun deleteWordCompletely(word: WordEntity){
+    fun deleteWordCompletely(word: WordEntity) {
         viewModelScope.launch {
             repository.deleteWordCompletely(word)
         }
     }
 
-    fun loadUnitsToAddTo(word: WordEntity){
+    fun loadUnitsToAddTo(word: WordEntity) {
         viewModelScope.launch {
             _unitsToAddTo.value = repository.getUnitsToAddTo(word)
         }
@@ -69,19 +84,51 @@ class UnitScreenModel(
     var dialogState by mutableStateOf<DialogState>(DialogState.None)
         private set
 
-    fun showDialog(state: DialogState){
+    fun showDialog(state: DialogState) {
         dialogState = state
     }
 
-    fun dismissDialog(){
+    fun dismissDialog() {
         dialogState = DialogState.None
     }
 
-    sealed class DialogState{
-        data object None: DialogState()
-        data object AddWordDialog: DialogState()
-        data class EditWordDialog(val word: WordEntity): DialogState()
-        data class DeleteWordDialog(val word: WordEntity): DialogState()
-        data class AddToAnotherUnitDialog(val word: WordEntity): DialogState()
+    fun nextSortMode(){
+        sortMode.value = SortMode.getNextMode()
+    }
+
+    sealed class DialogState {
+        data object None : DialogState()
+        data object AddWordDialog : DialogState()
+        data class EditWordDialog(val word: WordEntity) : DialogState()
+        data class DeleteWordDialog(val word: WordEntity) : DialogState()
+        data class AddToAnotherUnitDialog(val word: WordEntity) : DialogState()
+    }
+
+    sealed class SortMode(
+        nameRes: Long,
+        iconRes: Long
+    ) {
+        data object ByOriginalAsc : SortMode(0, 0)
+        data object ByOriginalDesc : SortMode(0, 0)
+        data object ByTranslationAsc : SortMode(0, 0)
+        data object ByTranslationDesc : SortMode(0, 0)
+        data object ByTimeAsc : SortMode(0, 0)
+        data object ByTimeDesc : SortMode(0, 0)
+
+        companion object {
+            private var currentIndex = 0
+            private val modes = listOf(
+                ByOriginalAsc, ByOriginalDesc,
+                ByTranslationAsc, ByTranslationDesc,
+                ByTimeAsc, ByTimeDesc
+            )
+
+            fun getNextMode(): SortMode {
+                if (currentIndex == modes.size) {
+                    currentIndex = 0
+                }
+                return modes[currentIndex++]
+            }
+        }
     }
 }
