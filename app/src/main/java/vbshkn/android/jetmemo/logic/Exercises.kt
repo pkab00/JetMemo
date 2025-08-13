d22bcf8cce451b13c77bec7bc1f9cb7e2978f033
vbshkn <pika_pikabu@vk.com>
2025-08-13 11:07:30 +0700

    Solved Exercise interface problem by adding common Answer interface

--- app/src/main/java/vbshkn/android/jetmemo/logic/Exercises.kt
+++ app/src/main/java/vbshkn/android/jetmemo/logic/Exercises.kt
@@ -1,18 +1,28 @@
 package vbshkn.android.jetmemo.logic
 
+sealed interface Answer {
+    data class WordPair(val str1: String, val str2: String) : Answer
+    data class YesNo(val answer: Boolean) : Answer
+}
+
 sealed interface Exercise {
     fun done(): Boolean
+    fun checkAnswer(answer: Answer): Boolean
 
-    class FourOptionsQuestion(
+    class RightOptionQuestion(
         val options: List<Word>,
         val correctAnswer: Word
     ) : Exercise {
         private var done = false
 
-        fun checkAnswer(str1: String, str2: String): Boolean {
-            done = (str1 == correctAnswer.original && str2 == correctAnswer.translation)
-                    || (str1 == correctAnswer.translation && str2 == correctAnswer.original)
-            return done
+        override fun checkAnswer(answer: Answer): Boolean {
+            if (answer is Answer.WordPair) {
+                val (str1, str2) = answer
+                done = (str1 == correctAnswer.original && str2 == correctAnswer.translation)
+                        || (str1 == correctAnswer.translation && str2 == correctAnswer.original)
+                return done
+            }
+            return false
         }
 
         override fun done(): Boolean {
@@ -25,18 +35,22 @@ sealed interface Exercise {
     ) : Exercise {
         val matched = mutableListOf<Word>()
 
-        fun checkAnswer(str1: String, str2: String): Boolean {
-            val variants = listOf(Word(str1, str2), Word(str2, str1))
-            variants.forEach { variant ->
-                if (options.contains(variant)) {
-                    matched.add(variant)
-                    return true
+        override fun checkAnswer(answer: Answer): Boolean {
+            if (answer is Answer.WordPair) {
+                val (str1, str2) = answer
+                val variants = listOf(Word(str1, str2), Word(str2, str1))
+                variants.forEach { variant ->
+                    if (options.contains(variant)) {
+                        matched.add(variant)
+                        return true
+                    }
                 }
+                return false
             }
             return false
         }
 
-        override fun done(): Boolean{
+        override fun done(): Boolean {
             return matched.containsAll(options)
         }
     }
@@ -47,9 +61,12 @@ sealed interface Exercise {
     ) : Exercise {
         var done = false
 
-        fun checkAnswer(answer: Boolean): Boolean {
-            done = (givenWord == correctWord) == answer
-            return done
+        override fun checkAnswer(answer: Answer): Boolean {
+            if (answer is Answer.YesNo) {
+                done = (givenWord == correctWord) == answer.answer
+                return done
+            }
+            return false
         }
 
         override fun done(): Boolean {
